86dd7a3520e417919a97a83e3289a0c3977fbad8
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-09-02 01:19:29 -0700

    done

--- 3th video practics/script.js
+++ 3th video practics/script.js
@@ -1,3 +1,40 @@
+// let path = `M 10 100 Q 100 100 890 100`
+
+// let final = `M 10 100 Q 100 100 890 100`
+
+
+// let string = document.querySelector(".string")
+
+// string.addEventListener("mousemove", function (dets) {
+//     let path = `M 10 100 Q 500 ${dets.y} 890 100`
+
+//     gsap.to("svg path", {
+//         attr: { d: path },
+//         duration: 2,
+//         ease: "power3.out"
+//     })
+// })
+
+// string.addEventListener("mouseleave", function () {
+
+//     gsap.to("svg path", {
+//         attr: { d: final },
+//         duration: 3,
+//         ease: "elastic.out(1,0.2)"
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
 let path = `M 10 100 Q 100 100 890 100`
 
 let final = `M 10 100 Q 100 100 890 100`
--- 4th video practics/script.js
+++ 4th video practics/script.js
@@ -1,3 +1,42 @@
+// let main = document.querySelector('#main');
+
+// let cursor = document.querySelector('#cursor');
+
+// let image = document.querySelector('#image');
+
+
+
+// main.addEventListener('mousemove', function (dets) {
+//     gsap.to(cursor, {
+//         y: dets.y,
+//         x: dets.x,
+//         duraction: 10,
+//         ease: "back.out"
+//     })
+
+// })
+
+// image.addEventListener("mouseenter", function () {
+//     gsap.to(cursor, {
+//         scale: 4,
+//     }
+//     )
+
+// })
+
+// image.addEventListener("mouseleave", function () {
+//     gsap.to(cursor, {
+//         scale: 1,
+//     }
+//     )
+
+// })
+
+
+
+
+
+
 let main = document.querySelector('#main');
 
 let cursor = document.querySelector('#cursor');
