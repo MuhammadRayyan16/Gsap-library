312f9ea895a136bfc68ace2ae237388be7e4a5ec
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-09-02 01:17:50 -0700

    done

--- 1th video practics/script.js
+++ 1th video practics/script.js
@@ -60,3 +60,31 @@ tl.from("h2", {
     scale: 0.2
 })
 
+
+// let tl = gsap.timeline()
+
+// tl.from("h1", {
+//     y: -30,
+//     opacity: 0,
+//     duration: 2,
+//     delay: 0.5,
+// })
+
+// tl.from("h4", {
+//     y: -30,
+//     opacity: 0,
+//     duration: 2,
+//     // delay: 1,
+//     stagger: 0.3
+// })
+
+
+
+// tl.from("h2", {
+//     y: -30,
+//     opacity: 0,
+//     duration: 0.5,
+//     delay: 1,
+//     scale: 0.2
+// })
+
